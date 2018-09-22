855883b1bbbc61ebb72e93c1f22c9b3ae5605d94
Gabriele Canepa <gabriele@canepa.io>
2018-09-22 17:06:29 +0200

    Add Babel with ES2015 preset

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,12 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
   }
 };
