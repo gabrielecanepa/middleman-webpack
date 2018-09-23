f8fa81dff840877100551c6c7c684979b9e1aa7a
Gabriele Canepa <gabriele@canepa.io>
2018-09-23 18:57:31 +0200

    Add dotenv and favicon gems

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'middleman'
 gem 'middleman-autoprefixer'
+gem 'middleman-dotenv'
+gem "middleman-favicon-maker"
 gem 'middleman-livereload'
 gem 'middleman-minify-html'
 gem 'middleman-pry'
--- config.rb
+++ config.rb
@@ -5,13 +5,13 @@ activate :autoprefixer do |prefix|
   prefix.browsers = 'last 2 versions'
 end
 
+activate :dotenv
+
 activate :external_pipeline,
          name:     :webpack,
          command:  build? ? 'yarn run build' : 'yarn run start',
          source:   'dist',
          latency:  1
-
-set :relative_links, true
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
@@ -54,8 +54,39 @@ configure :development do
 end
 
 configure :build do
+  ignore 'javascripts/*' # handled by webpack
+  set :relative_links, true
   activate :minify_html
   activate :minify_css
   activate :minify_javascript
-  ignore 'javascripts/*'
+  activate :favicon_maker do |f|
+    f.template_dir  = 'source/images'
+    f.icons = {
+      "logo.svg" => [
+        { icon: "apple-touch-icon-180x180-precomposed.png" },
+        { icon: "apple-touch-icon-152x152-precomposed.png" },
+        { icon: "apple-touch-icon-144x144-precomposed.png" },
+        { icon: "apple-touch-icon-120x120-precomposed.png" },
+        { icon: "apple-touch-icon-114x114-precomposed.png" },
+        { icon: "apple-touch-icon-76x76-precomposed.png" },
+        { icon: "apple-touch-icon-72x72-precomposed.png" },
+        { icon: "apple-touch-icon-60x60-precomposed.png" },
+        { icon: "apple-touch-icon-57x57-precomposed.png" },
+        { icon: "apple-touch-icon-precomposed.png", size: "57x57" },
+        { icon: "apple-touch-icon.png", size: "57x57" },
+        { icon: "favicon-196x196.png" },
+        { icon: "favicon-160x160.png" },
+        { icon: "favicon-96x96.png" },
+        { icon: "favicon-32x32.png" },
+        { icon: "favicon-16x16.png" },
+        { icon: "favicon.png", size: "16x16" },
+        { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+        { icon: "mstile-70x70.png", size: "70x70" },
+        { icon: "mstile-144x144.png", size: "144x144" },
+        { icon: "mstile-150x150.png", size: "150x150" },
+        { icon: "mstile-310x310.png", size: "310x310" },
+        { icon: "mstile-310x150.png", size: "310x150" }
+      ]
+    }
+  end
 end
