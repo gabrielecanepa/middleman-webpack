c3da851c31f2836c41ca2d0ec364c15c2db948fe
Gabriele Canepa <gabriele@canepa.io>
2018-09-29 00:56:34 +0200

    Update main entry

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    main: './source/javascripts/application.js'
+    main: './source/assets/javascripts/application.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
