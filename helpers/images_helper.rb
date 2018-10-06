588f67e75af80b097d8461875af1cf02d0e4611f
Gabriele Canepa <gabriele@canepa.io>
2018-10-06 18:52:27 +0200

    Add inline SVG images and helper

---
+++ helpers/images_helper.rb
@@ -0,0 +1,11 @@
+require 'lib/svg'
+
+module ImagesHelper
+  def svg_tag(filename, options = {})
+    root      = Middleman::Application.root
+    file_path = File.join(root, 'source', config[:images_dir], filename)
+    return '(SVG image not found)' unless File.exist?(file_path)
+
+    SVG.inline(file_path, options)
+  end
+end
