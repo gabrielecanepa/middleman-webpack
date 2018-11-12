32bf9f8bf4e5ccf154308aa768e817b1f41baabc
Gabriele Canepa <gabriele@canepa.io>
2018-11-12 23:09:48 +0000

    Update Babel configuration

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -12,9 +12,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
         test: /\.css$/,
