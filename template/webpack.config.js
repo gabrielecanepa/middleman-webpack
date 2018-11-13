d5f5b07de4f6b5b7227c9c1f0cb6984dd200e247
Gabriele Canepa <gabriele@canepa.io>
2018-11-13 00:16:19 +0000

    Add separate Babel config

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -14,12 +14,7 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        loader: "babel-loader"
       },
       {
         test: /\.css$/,
