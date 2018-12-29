568095379ee6eb7e4783f9e32ea0c38f3cf218ce
Gabriele Canepa <gabriele@canepa.io>
2018-12-29 16:27:49 +0100

    Rename main JavaScript entry

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: {
-    main: "./source/assets/javascripts/all.js"
+    main: "./source/assets/javascripts/index.js"
   },
   output: {
     path: path.resolve(__dirname, "dist"),
